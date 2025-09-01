9d4f4334c2c1fd6ac49e53fc267b932c12acbd89
dynamicer <kfxhjz@gmail.com>
2025-09-01 10:43:32 +0800

    wezterm support OPT-Left and OPT-Right for macos

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,11 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices.
-
--- For example, changing the initial geometry for new windows:
+-- changing the initial geometry for new windows:
 config.initial_cols = 120
 config.initial_rows = 28
 
@@ -17,6 +16,23 @@ config.font = wezterm.font_with_fallback({
 	"NotoMono Nerd Font Mono",
 })
 config.font_size = 14
+
+config.keys = {
+	-- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act.SendKey({
+			key = "b",
+			mods = "ALT",
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act.SendKey({ key = "f", mods = "ALT" }),
+	},
+}
 
 -- Finally, return the configuration to wezterm:
 return config
